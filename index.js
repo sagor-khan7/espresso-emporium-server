d53edeacdfaa89fb8af38437738650c69d67905f
sagor-khan7 <sksagor791@gmail.com>
2025-08-18 21:28:34 +0600

    delete a coffee data from database

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -41,6 +41,14 @@ async function run() {
     app.post("/coffees", async (req, res) => {
       const formData = req.body;
       const result = await coffeesCollection.insertOne(formData);
+      res.send(result);
+    });
+
+    //? delete a coffee data
+    app.delete("/coffees/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await coffeesCollection.deleteOne(query);
       res.send(result);
     });
 
